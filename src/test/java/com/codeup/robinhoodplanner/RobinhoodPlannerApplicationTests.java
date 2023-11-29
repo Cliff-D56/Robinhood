5dfc578d65142bbf55f0fe1b2dbb2ce530a52c4d
Clifford Gardner <cliffordgardner9@gmail.com>
2023-11-29 14:09:47 -0600

    test

---
+++ src/test/java/com/codeup/robinhoodplanner/RobinhoodPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.robinhoodplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobinhoodPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
